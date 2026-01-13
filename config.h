5cb0c9a1966f9460ddccbdd4cc50ea99a895c363
Somma <gustavosferracini@Proton.me>
2026-01-13 19:00:47 -0300

    Rename baseOrigem to origemBase in Config struct

--- config.h
+++ config.h
@@ -2,11 +2,6 @@
 #include <string>
 
 struct Config {
-    std::string baseOrigem;
+    std::string origemBase;
     std::string destino;
-    int intervaloMs;
 };
-
-bool loadConfig(Config& cfg);
-void createConfigWizard(Config& cfg);
-std::string getConfigPath();
